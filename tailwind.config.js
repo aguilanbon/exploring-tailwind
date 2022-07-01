edf61c6652b33f4bd89cde536c4be52d481b7f2e
aguilanbon <aguilanbon@gmail.com>
2022-07-01 15:53:07 +0800

    left column of fb clone done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,23 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
-    colors: {
+    extend: {
+      colors: {
       'fb-background' : '#F0F2F5',
       'fb-logo' : '#1877F2',
       'fb-text-gray': '#8A8D91',
-      'fb-border-color': '#CCD0D5'
-    }
+      'fb-border-color': '#CCD0D5',
+      'fb-border-color2': '#DADDE1',
+      'fb-border-color3': '#DDDFE2'
+    },
+      boxShadow: {
+        'try': '0 0 10px #CCD0D5',
+        'try2': '0 0 20px #CCD0D5',
+      },
+      fontSize: {
+        'xxs': '.65rem'
+      }
+    },
   },
   plugins: [],
 }
